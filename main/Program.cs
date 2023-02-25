63330f89f2006600c7543396a5fa4a8823858715
Kirill Bulekov <kirya.bulekov@mail.ru>
2023-02-25 19:40:10 +0300

    Method StrToArray added

---
+++ main/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] StrToArray(string input)
+{
+    int count = 1;
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i] == ',')
+        {
+            count++;
+        }
+    }
+
+    string[] num = new string[count];
+    int index = 0;
+
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ',')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        num[index] = Convert.ToString(temp);
+        index++;
+    }
+    return num;
+}
+
